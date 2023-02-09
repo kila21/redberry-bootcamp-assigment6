86bb219a7b93d3180d3e40dc2e1a825b2a62da0d
kila <luka.kilasonia.1@btu.edu.ge>
2023-02-09 17:43:51 +0400

    [info.jsx] add custom validations and styles

--- src/components/info/info.jsx
+++ src/components/info/info.jsx
@@ -2,6 +2,7 @@ import { useForm } from 'react-hook-form'
 import {useState, useEffect} from 'react'
 
 import './info.css'
+import valid from '../../assets/images/valid.svg'
 
 
 const Info = (props) => {
@@ -14,16 +15,37 @@ const Info = (props) => {
         mobile: '',
     });
 
-    const {register, handleSubmit, formState: {errors}} = useForm({mode: 'all'})
+    const {register, handleSubmit, formState: {errors,touchedFields}} = useForm({mode: 'all'})
  
     useEffect(()=>{
         props.update(data)
-
     },[data])
 
-    console.log(errors)
+    //ქასთომ ვალიდაციები
+    const validateGeorgianLetter = (value) => {
+        const georgianCharacters = /^[ა-ჰ]+$/gm
+        return  new RegExp(georgianCharacters).test(value)  
+    }
+
+    const validateEmail = (value) => {
+        return value.endsWith('@redberry.ge')
+    }
+
+    const validateNumber = (value) => {
+        const georgianNunmber = /^\+995\s5\d{2}\s\d{2}\s\d{2}\s\d{2}$/ 
+
+        return new RegExp(georgianNunmber).test(value)
+    }
+
+    const addNumberSpace = (e) => {
+        if(e.target.value.length === 3) {
+            // console.log(e.target.value)
+            // e.target.value += ' '
+        }
+    }
 
     const getImgValueFromInput =  (event) => {
+        console.log('img')
         if (event.target.files && event.target.files[0]) {
             setData({
                 ...data,img: URL.createObjectURL(event.target.files[0])
@@ -34,8 +56,7 @@ const Info = (props) => {
 
     return (
         <form className='info' onSubmit={handleSubmit((data) => {
-            console.log(data)
-            setData(data)
+            // setData(data)
         })}>
             <div className='back'>
                 &lt;
@@ -50,39 +71,53 @@ const Info = (props) => {
                 <div className='info-person__name'>
                     <label htmlFor='name'>სახელი</label>
                     <input
+                        className={errors.firstName?.message ? 'error' : ' '}
                         id='name' 
                         onKeyUp={(e) => setData({...data,firstName: e.target.value})} 
                         type='text' 
                         {
                             ...register('firstName', 
                             {required: 'მინიმუმ 2 ასო, ქართული ასოები', 
-                            minLength: {value: 2, message: 'მინიმუმ 2 ასო, ქართული ასოები'}})
+                            minLength: {value: 2, message: 'მინიმუმ 2 ასო, ქართული ასოები'},
+                            validate: (value) => validateGeorgianLetter(value) || 'მინიმუმ 2 ასო, ქართული ასოები'})
                         }
                     />
                     <p>{errors.firstName?.message}</p>
+                    {(!errors.firstName?.message && touchedFields.firstName) && (<img className='valid' src={valid} alt='valid.svg'/>)}
                 </div>
                
                 <div className='info-person__surname'>
                     <label htmlFor='lastname'>გვარი</label>
-                    <input id='lastname'
+                    <input 
+                        id='lastname'
+                        className={errors.lastName?.message ? 'error' : ' '}
                         onKeyUp={(e) => setData({...data,lastName: e.target.value})}
                         type='text'
                         {
                             ...register('lastName',
                             {required: 'მინიმუმ 2 ასო, ქართული ასოები',
-                            minLength: {value: 2, message: 'მინიმუმ 2 ასო, ქართული ასოები'}})
+                            minLength: {value: 2, message: 'მინიმუმ 2 ასო, ქართული ასოები'},
+                            validate: (value) => validateGeorgianLetter(value) || 'მინიმუმ 2 ასო, ქართული ასოები'
+                        })
                         }
                     />
                     <p>{errors.lastName?.message}</p>
+                    {/* <img className='valid' src={valid} alt='valid.svg'/> */}
+                    {(!errors.lastName?.message && touchedFields.lastName) && (<img className='valid' src={valid} alt='valid.svg'/>)}
                 </div>
                 
 
             </div>
             
             <div className='info-img'>
-                <label htmlFor='upload'>პირადი ფოტოს ატვირთვა</label>
+                <label className={errors.img?.message ? 'error' : ' '} htmlFor='upload'>პირადი ფოტოს ატვირთვა</label>
                 <label className='input' htmlFor='upload'>ატვირთვა</label>
-                <input type='file' id='upload' onChange={(event) => getImgValueFromInput(event)} />
+                <input
+                 type='file' 
+                 id='upload' 
+                 {...register('img', {required: 'ატვირთეთ ფოტო'})}
+                 onChange={(event) => getImgValueFromInput(event)} 
+                 />
                 {/* <div  for='upload'></div> */}
             </div>
 
@@ -94,22 +129,38 @@ const Info = (props) => {
             <div className='info-email'>
                 <label htmlFor='email'>ელ.ფოსტა</label>
                 <input 
+                    className={errors.email?.message ? 'error' : ' '}
                     id='email' 
                     type='email' 
-                    {...register('email', {required: 'უნდა მთავრდებოდეს @redberry.ge-ით'})}
+                    {...register('email',
+                        {required: 'უნდა მთავრდებოდეს @redberry.ge-ით',
+                        validate: (value) => validateEmail(value) || 'უნდა მთავრდებოდეს @redberry.ge-ით'
+                        })
+                    }
                     onKeyUp={(e) => setData({...data,email: e.target.value})} 
                 />
                 <p>{errors.email?.message}</p>
+                {(!errors.email?.message && touchedFields.email) && (<img className='valid' src={valid} alt='valid.svg'/>)}
             </div>
 
             <div className='info-mobile'>
                 <label>მობილურის ნომერი</label>
                 <input
-                    type="number" 
-                    {...register('mobile', {required: 'უნდა აკმაყოფილებდეს ქართული მობილურის ნომრის ფორმატს'})}
+                    className={errors.mobile?.message ? 'error' : ' '}
+                    type="text" 
+                    // onKeyDown={(e) => addNumberSpace(e)}
+                    {...register('mobile', 
+                        {required: 'უნდა აკმაყოფილებდეს ქართული მობილურის ნომრის ფორმატს',
+                         validate: (value) => validateNumber(value) || 'უნდა აკმაყოფილებდეს ქართული მობილურის ნომრის ფორმატს'
+                        })
+                
+                    }
+
                     onKeyUp={(e) => setData({...data,mobile: e.target.value})} 
                     />
                 <p>{errors.mobile?.message}</p>
+               
+                {(!errors.mobile?.message && touchedFields.mobile) && (<img className='valid' src={valid} alt='valid.svg'/>)}
             </div>
 
             <div className='info-submit'>
