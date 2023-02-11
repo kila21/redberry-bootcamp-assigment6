ea175e345f1a3e84a29beacaf46c2c0326a1d261
kila <luka.kilasonia.1@btu.edu.ge>
2023-02-11 17:20:00 +0400

    add get method for degrees

--- src/components/education/educationForm.jsx
+++ src/components/education/educationForm.jsx
@@ -1,17 +1,18 @@
 import { useEffect, useState } from "react"
 import { useForm } from "react-hook-form"
+import axios from 'axios'
 
 import './educationForm.css'
 
 const EducationForm =  (props) => {
+    const url = 'https://resume.redberryinternship.ge/api/degrees'
     const [educationData, setEducationData] = useState(
         {institute: '', degree: '', due_date: '', description: ''}
     )
+    const [degrees, setDegrees] = useState()
 
     const getDataFromStorage = JSON.parse(sessionStorage.getItem('educationFormsData'));
-    // console.log(getDataFromStorage,getDataFromStorage[props.index]?.institute)
-
-    const {register, formState: {errors,isValid}, getValues} = useForm({
+    const {register, formState: {errors,isValid, touchedFields}, getValues} = useForm({
         mode: 'all',
         defaultValues: async () => {
             setEducationData({
@@ -35,6 +36,11 @@ const EducationForm =  (props) => {
         props.update(props.index, educationData, isValid)
     },[educationData])
 
+    useEffect(()=> {
+        axios.get(url).then((data)=>{
+            setDegrees(data.data)
+        })
+    },[])
     return (
         <form className='education'>
             <div className='education-school'>
@@ -43,7 +49,8 @@ const EducationForm =  (props) => {
                 type="text" 
                 id='institute' 
                 placeholder='სასწავლებელი'
-                {...register('institute',{required: 'მინიმუმ 2 სიმბოლო'})}
+                className={!errors?.institute && touchedFields?.institute ? 'validEducationInput' : ''}
+                {...register('institute',{required: 'მინიმუმ 2 სიმბოლო', minLength: 2})}
                 onKeyUp={(e) => setEducationData({...educationData,institute: e.target.value}) }
                 
                 />
@@ -57,15 +64,27 @@ const EducationForm =  (props) => {
                     {...register('degree',{required: true})}
                     onInput={(e) => setEducationData({...educationData,degree: e.target.value})} 
                     type="select"
-                    id='degree' >
-                        <option value="ბაკალავრი">ბაკალავრი</option>
-                        <option value='სტუდენტი'>სტუდენტი</option>
+                    id='degree' 
+                    className={!errors?.degree && !touchedFields?.degree ? '' : 'validEducationInput'}
+                    >
+                        <option value="" disabled selected hidden>აირჩიეთ ხარისხი</option>
+                        {/* <option value="ბაკალავრი">ბაკალავრი</option>
+                        <option value='სტუდენტი'>სტუდენტი</option> */}
+                        {degrees?.map((item)=>{
+                            return <option key={item.id} value={item.title}>{item.title}</option>
+                        })}
                     </select>
                 </div>
 
                 <div className='education-date'>
                     <label htmlFor='date'>დამთავრების რიცხვი</label>
-                    <input {...register('due_date', {required: true})} onChange={(e)=> setEducationData({...educationData,due_date: e.target.value})} type="date" id='date' />
+                    <input 
+                    {...register('due_date', {required: true})} 
+                        onChange={(e)=> setEducationData({...educationData,due_date: e.target.value})} 
+                        type="date" 
+                        id='date' 
+                        className={!errors?.due_date && touchedFields.due_date ? 'validEducationInput': ''}
+                        />
                 </div>
             </div>
 
@@ -75,7 +94,9 @@ const EducationForm =  (props) => {
                     {...register('description',{required: 'აუცილებელია'})}
                     onKeyUp={(e) => setEducationData({...educationData,description: e.target.value}) } 
                     id='textarea' 
-                    placeholder='განათლების აღწერა'></textarea>
+                    placeholder='განათლების აღწერა'
+                    className={!errors?.description && touchedFields.description ? 'validEducationInput': ''}
+                    ></textarea>
             </div>
     </form>
     )
