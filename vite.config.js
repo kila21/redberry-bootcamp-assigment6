589138e69c18f92ce25716c42c92a8f1353777b9
kila <luka.kilasonia.1@btu.edu.ge>
2023-03-06 18:03:54 +0400

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/redberry-bootcamp-assigment6/',
+  base: '/redberry-bootcamp-assigment6/dist',
   plugins: [react()],
 })
