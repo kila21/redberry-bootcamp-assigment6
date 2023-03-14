bbbca62889695f47e6ce00d9fecf5f786764d6df
kila <luka.kilasonia.1@btu.edu.ge>
2023-03-14 19:53:23 +0400

    remope base

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/redberry-bootcamp-assigment6/',
+  // base: '/redberry-bootcamp-assigment6/',
   plugins: [react()],
-  build: {
-    root: 'src',
-    outDir: './dist',
-    emptyOutDir: true
-  }
+  
 })
